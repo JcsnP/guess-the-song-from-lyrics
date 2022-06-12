5a6eec2106543348d677b0286d906ebedcabad89
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2022-06-13 02:14:58 +0700

    add easter egg

---
+++ greetingInConsole.js
@@ -0,0 +1,5 @@
+console.log('---------------------------');
+console.log('%c เห็นนะว่าแอบส่องอ่ะ' + '%c หาไรอยู่หรอ ?',  'background: #FFA500; color: #444', 'background: #FFA500; color: #000');
+console.log('%c https://youtu.be/dQw4w9WgXcQ', 'background: #FFF; color: #FFF');
+console.log('%c ส่งไปเถอะ โค้ดมันเน่ามาก 😥', 'background: #FF0000; color: #FFF');
+console.log('---------------------------');
